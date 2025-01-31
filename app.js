64ff4947b483ba120a2449be866a2be0cb1253db
Daniel Cortes <jadanielc@gmail.com>
2025-01-30 19:59:10 -0500

    actualizacion lista de amigo y generacion de lista de los amigos en el DOM

--- app.js
+++ app.js
@@ -10,12 +10,29 @@ function agregarAmigo(){
         alert("Por favor, inserte un nombre.")
     }else{
     amigos.push(nombre);
+    actualizarListaAmigos();
     console.log(amigos)
     limpiarCampo();
+    
     }
 }
 
-//funcion que limpia la caja
+//funcion que limpia la caja al ingresar el nombre. 
 function limpiarCampo(){
     document.getElementById('amigo').value ="";
+}
+
+
+//funcion para generar la lista 
+function actualizarListaAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML ="";
+    let fragmento = document.createDocumentFragment(); //creacion de fragmento en memoria
+
+    amigos.forEach(amigo => {
+    let li = document.createElement('li'); //crea elemento li
+    li.textContent = amigo; // agrega el texto
+    fragmento.appendChild(li);
+    });
+    lista.appendChild(fragmento);
 }
