7417b14cec035eb408227f9c767149b4f19ec2bb
Daniel Cortes <jadanielc@gmail.com>
2025-01-30 19:00:43 -0500

    implementacion de una funcion para agregar amigos y limpiar el input

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+
+//funcion para agregar amigo al juego.
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    if(nombre === ""){
+        alert("Por favor, inserte un nombre.")
+    }else{
+    amigos.push(nombre);
+    console.log(amigos)
+    limpiarCampo();
+    }
+}
+
+//funcion que limpia la caja
+function limpiarCampo(){
+    document.getElementById('amigo').value ="";
+}
